0364be70d6cd9abcaa91e8ae36c6edf3bcebea85
khashayardadashi <115826321+khashayardadashi@users.noreply.github.com>
2022-10-23 20:40:58 +0330

    Update roman_number.py

--- EX1/roman_number.py
+++ EX1/roman_number.py
@@ -1,1 +1,23 @@
-
+def EX1_6 (number):
+    if number== 1 :
+        return 'I'
+    elif  number== 2:
+        return 'II'
+    elif  number== 3:
+        return 'III'
+    elif  number== 4:
+        return 'IV'
+    elif number== 5:
+        return' V'
+    elif number== 6:
+        return 'VI'
+    elif number== 7:
+        return 'VII'
+    elif number==8:
+        return 'VIII'
+    elif number==9:
+        return 'IX'
+    elif number==10:
+        return 'X'
+    else:
+        return 'Error'
