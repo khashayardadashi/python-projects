d2c26433db07a44d3d759aa667f3a5ee622ff33a
khashayardadashi <115826321+khashayardadashi@users.noreply.github.com>
2022-10-23 20:42:10 +0330

    Update age_classifier.py

--- EX1/age_classifier.py
+++ EX1/age_classifier.py
@@ -1,1 +1,9 @@
-
+def EX1_5(person_age):
+    if person_age<=1 :
+        return(f'he or she is an infant')
+    elif person_age>1 and person_age<13:
+        return(f'he or she is a child')
+    elif person_age>=13 and person_age<20:
+        return(f'he or she is a teenage')
+    elif person_age>=20:
+        return(f'he or she is an adualt')
