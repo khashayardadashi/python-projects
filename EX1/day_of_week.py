ca31ffc4d3eec8e67ab82e46f54920d89b95dedc
khashayardadashi <115826321+khashayardadashi@users.noreply.github.com>
2022-10-23 20:38:20 +0330

    Update day_of_week.py

--- EX1/day_of_week.py
+++ EX1/day_of_week.py
@@ -1,1 +1,18 @@
+def EX1_4 (number):
+    if number== 1 :
+        return 'monday'
+    elif  number== 2:
+        return 'tuesday'
+    elif  number== 3:
+        return 'wednesday'
+    elif  number== 4:
+        return 'thursday'
+    elif number== 5:
+        return' friday'
+    elif number== 6:
+        return 'saturday'
+    elif number== 7:
+        return 'sunday'
+    else:
+        return 'Error'
 
